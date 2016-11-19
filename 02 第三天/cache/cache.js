7c401bf6e6ee22b57e53f9e40ee786892d26b055
lktsk <18195541105@163.com>
2016-11-19 10:01:52 +0800

    add HTML5-Learning-notes

---
+++ 02 第三天/cache/cache.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2016/8/24.
+ */
+window.onload=function () {
+    var btn=document.getElementsByTagName("button")[0];
+    btn.onclick=function () {
+        alert("1321");
+    }
+};
